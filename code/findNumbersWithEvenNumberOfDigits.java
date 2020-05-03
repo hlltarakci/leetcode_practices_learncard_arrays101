c019afc41f51fda77e74bff72f8767970af58d76
Hilal Tarakci <hlltarakci@gmail.com>
2020-05-03 14:52:21 +0300

    Create  findNumbersWithEvenNumberOfDigits.java

---
+++ code/findNumbersWithEvenNumberOfDigits.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/explore/learn/card/fun-with-arrays/521/introduction/3237/
+
+class Solution {
+    /*
+        n: num on numbers
+        k: num of digits on max num
+        time: O(n log k)
+        space: O(1)
+    */
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for(int num: nums) {
+            if( isEvenNumOfDigits(num) ) count++;
+        }
+        
+        return count;
+    }
+    
+    private boolean isEvenNumOfDigits(int n) {
+        int sum = 0;
+        while(n > 0) {
+            sum++;
+            n /= 10;
+        }
+        
+        return sum % 2 == 0;
+    }
+}
