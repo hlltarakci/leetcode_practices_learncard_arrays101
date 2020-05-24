e2686355871bad05c9e6985cd7935b06898f65c8
Hilal Tarakci <hlltarakci@gmail.com>
2020-05-24 16:31:46 +0300

    Update removeElement.java

--- code/removeElement.java
+++ code/removeElement.java
@@ -1,11 +1,31 @@
-// https://leetcode.com/explore/learn/card/fun-with-arrays/526/deleting-items-from-an-array/3247/
-
 class Solution {
-    /*
+    /* 
+        if elements to remove are rare, this is more efficient
         n: array length
         time: O(n)
         space: O(1)
     */
+    public int removeElement(int[] nums, int val) {
+        if(nums.length == 0) return 0;
+        
+        int left = 0, right = nums.length-1;
+        while(left < right) {
+            if(nums[left] == val) {
+                nums[left] = nums[right];
+                nums[right] = val;
+                right--;
+            } else left++;
+        }
+        
+        return nums[left] == val ? left : left+1;
+    }
+
+    /* 
+        trivial approach
+        n: array length
+        time: O(n)
+        space: O(1)
+    
     public int removeElement(int[] nums, int val) {
         int realIndex = 0;
         
@@ -18,4 +38,5 @@ class Solution {
         
         return realIndex;
     }
+    */
 }
